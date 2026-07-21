72459ebd20b8d153fcb861b51bc5875bf1c9a6be
kompoti <253134458+kompoti121@users.noreply.github.com>
2026-07-21 04:32:36 -0400

    release: v0.1.28 (29)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.miruronative"
         minSdk = 26
         targetSdk = 36
-        versionCode = 28
-        versionName = "0.1.27"
+        versionCode = 29
+        versionName = "0.1.28"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
